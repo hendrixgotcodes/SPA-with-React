3c93b491954ed883b9e5a1a8ad7138657b0573d4
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-02-02 16:12:17 +0000

    Added tasks component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import Header from "./components/_header";
 import Task from './components/_tasks';
 
 function App() {
+
+  const tasks = [
+
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id:2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 3rd at 2:30pm',
+        reminder: false
+    }
+
+  ]
+
+
   return (
     <div className="center">
 
       <div className="container">
         
           <Header/>
-          <Task/>
+          <Task tasks={tasks}/>
 
       </div>
 
--- src/components/_tasks.js
+++ src/components/_tasks.js
@@ -1,29 +1,6 @@
 import React from 'react';
 
-const tasks = [
-
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id:2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 3rd at 2:30pm',
-        reminder: false
-    }
-
-]
-
-const Task = ()=>{
+const Task = ({tasks})=>{
 
     return (
         <>
